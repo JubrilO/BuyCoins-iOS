094698a370aeaa1c01844435b685a8fed8709682
Jubril O <jubrylola@gmail.com>
2018-05-22 05:41:16 +0100

    Initial Commit

---
+++ BuyCoins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuyCoins
+//
+//  Created by Jubril on 22/05/2018.
+//  Copyright © 2018 bitkoin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
